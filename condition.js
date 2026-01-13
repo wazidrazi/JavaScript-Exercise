58a9164a4aaa0161e4ec69f386b4d45493b38c0c
wazidrazi <wazidrazi20@gmail.com>
2026-01-13 22:06:14 +0600

    condtition

---
+++ condition.js
@@ -0,0 +1,19 @@
+// var totalPassenger = 10;
+// if(totalPassenger < 10){
+//     console.log("We can not go to saint martin");
+// }else{
+//     console.log("We can go to Saint Martin");
+// }
+
+
+
+var hasJob = false;
+var savings = 50000;
+
+if(hasJob === false && savings > 30000){
+    console.log("Ready to start a business");
+} else if(hasJob === true || savings > 100000){
+    console.log("Focus on the job");
+}else{
+    console.log("You are a loser");
+}
--- practice-js.js
+++ practice-js.js
@@ -1,13 +1,36 @@
-var promise = "I will complete javascript within 3 months";
-console.log(promise.toUpperCase());
+// var promise = "I will complete javascript within 3 months";
+// console.log(promise.toUpperCase());
 
-var promise = "I will complete javascript within 3 months";
-console.log(promise.indexOf('javascript'));
+// var promise = "I will complete javascript within 3 months";
+// console.log(promise.indexOf('javascript'));
 
 
-var num1 = 0.1;
-var num2 = 0.2;
+// var num1 = 0.1;
+// var num2 = 0.2;
 
-var total = num1 + num2;
-total = total.toFixed(3);
-console.log(total);
+// var total = num1 + num2;
+// total = total.toFixed(3);
+// console.log(total);
+
+
+// var price1 = 35;
+// var price2 = 45;
+
+
+// console.log(price1 + price2);
+// console.log(price2 - price1);
+// console.log(price1 * price2);
+// console.log(price1 / price2);
+// console.log(price2 % price1);
+
+
+// var number = 5.3444;
+// var result = Math.round(number);               //round, ceil, floor
+
+// console.log(result);
+
+
+var number = Math.random() * 100;
+var result = Math.round(number);                //pick random numbers for lottery
+
+console.log(result);

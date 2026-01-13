3a047f04526201ad0f4573e26e657573d89d46ed
wazidrazi <wazidrazi20@gmail.com>
2026-01-13 20:58:37 +0600

    first commit

---
+++ practice-js.js
@@ -0,0 +1,13 @@
+var promise = "I will complete javascript within 3 months";
+console.log(promise.toUpperCase());
+
+var promise = "I will complete javascript within 3 months";
+console.log(promise.indexOf('javascript'));
+
+
+var num1 = 0.1;
+var num2 = 0.2;
+
+var total = num1 + num2;
+total = total.toFixed(3);
+console.log(total);

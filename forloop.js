27ec87e040867c638313b3bf8ab18396c65746cd
wazidrazi <wazidrazi20@gmail.com>
2026-01-14 22:03:57 +0600

    for loop

---
+++ forloop.js
@@ -0,0 +1,15 @@
+var numbers = [14, 50, 23, 45, 38, 90, 39];
+
+for (i = 0; i < numbers.length; i++){
+    var elements = numbers[i];
+    console.log(elements);
+}
+
+                               // or
+
+                               
+var num = [1, 2, 3, 4];
+
+for(i=0; i < num.length; i++){
+    console.log(num[i]);
+}

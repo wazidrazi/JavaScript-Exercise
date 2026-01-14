3a552d67c7c0597f0b1f0fe486248c0bae9c8a8f
wazidrazi <wazidrazi20@gmail.com>
2026-01-14 21:25:12 +0600

    array

---
+++ array.js
@@ -0,0 +1,25 @@
+var studentAge = [15, 18, 20, 25];
+
+var wazidsAge = studentAge[2];        // find value of index
+
+var position = studentAge.indexOf(20);  // find index number
+
+
+console.log(studentAge.length);            // to see total element in an array
+
+studentAge.push(26);             // to insert an element
+console.log(studentAge);
+
+studentAge.unshift(15, 33);        // to add element at the beginning
+
+studentAge.pop();                   // to delete
+console.log(studentAge);
+
+studentAge.shift();                 // detete first element at the beginning
+console.log(studentAge);
+
+
+
+
+
+
--- condition.js
+++ condition.js
@@ -7,13 +7,13 @@
 
 
 
-var hasJob = false;
-var savings = 50000;
+// var hasJob = false;
+// var savings = 50000;
 
-if(hasJob === false && savings > 30000){
-    console.log("Ready to start a business");
-} else if(hasJob === true || savings > 100000){
-    console.log("Focus on the job");
-}else{
-    console.log("You are a loser");
-}
+// if(hasJob === false && savings > 30000){
+//     console.log("Ready to start a business");
+// } else if(hasJob === true || savings > 100000){
+//     console.log("Focus on the job");
+// }else{
+//     console.log("You are a loser");
+// }

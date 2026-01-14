299a0f553fffbbcc29d0aada670922eaacdebfac
wazidrazi <wazidrazi20@gmail.com>
2026-01-14 23:07:20 +0600

    object

---
+++ object.js
@@ -0,0 +1,16 @@
+var student1 = {
+    name : "Wazid", age: 25, occupation: "unemployed"
+};
+var student2 = {
+    name : "Rahim", age: 23, occupation: "student"
+};
+var student3 = {
+    name : "karim", age: 29, occupation: "Business"
+};
+
+student1.occupation = "Service holder";               // change property value
+student1.hobby = "Watching movies";                 // add new property
+
+student3.income = 50000;
+console.log(student1);
+console.log(student3);

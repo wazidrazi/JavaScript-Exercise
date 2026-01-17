304becea18b2e583ee1adcd3620140aaa099b7e3
wazidrazi <wazidrazi20@gmail.com>
2026-01-17 21:02:31 +0600

    new

---
+++ findMax.js
@@ -0,0 +1,27 @@
+// let wazid = 970;
+// let rahim = 890;
+// let karim = 960;
+
+// if(wazid > rahim){
+//     if(wazid > karim){
+//         console.log("Wazid has much money");
+//     }else{
+//         console.log("Karim has much money");
+//     }
+// }else{
+//     if(rahim > karim){
+//         console.log("Rahim has much money");
+//     }else{
+//         console.log("Karim has much money");
+//     }
+// }
+
+
+let wazid = 970;
+let rahim = 890;
+let karim = 960;
+
+let findMax = Math.max(wazid, rahim, karim);
+console.log(findMax);
+
+
---
+++ randomNumber.js
@@ -0,0 +1,12 @@
+let randomNum = Math.random() * 6;
+let result = Math.round(randomNum);
+
+console.log(result);
+
+
+for(let i = 0; i <= 6; i++){
+    let randomNum = Math.random() * 6;
+     result = Math.round(randomNum);
+
+    console.log(result);
+}

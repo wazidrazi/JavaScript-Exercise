aa301fc4bcbabeecd73168d1e988d69e39d71337
wazidrazi <wazidrazi20@gmail.com>
2026-01-17 20:32:48 +0600

    swap number

---
+++ swap.js
@@ -0,0 +1,34 @@
+// let a = 7;
+// let b = 9;
+
+// console.log("Before Swap: a =", a, "b =", b);
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log("After Swap: a = ", a, "b =",b);
+
+
+
+
+// let x = 7;
+// let y = 9;
+
+// console.log("Before Swap: x =", x, "y =", y);
+
+// x = x + y;
+// y = x - y;
+// x = x - y;
+
+// console.log("After Swap: x =", x, "y =", y);
+
+
+
+let p = 7;
+let q = 9;
+console.log("Before Swap: p =", p, "q =", q);
+
+[p,q] = [q,p];
+
+console.log("After Swap: p =", p, "p =", q);

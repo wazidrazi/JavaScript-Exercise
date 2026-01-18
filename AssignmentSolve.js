93c58eb261cdca4d9cd9a22bfbc191c271c48393
wazidrazi <wazidrazi20@gmail.com>
2026-01-18 22:05:19 +0600

    Assignments

---
+++ AssignmentSolve.js
@@ -0,0 +1,71 @@
+/* Assignment 1 */
+
+
+function feetToMile(feet){
+    let mile = feet / 5280;
+    
+    return mile.toFixed(4);
+}
+
+console.log(feetToMile(100));
+
+
+/* Assignment 2 */
+
+function woodCalculator(chair, table, bed){
+    let chairsWood = chair * 1;
+    let tablesWood = table * 3;
+    let BedsWood = bed * 5;
+
+    let total = chairsWood + tablesWood + BedsWood;
+    return total;
+}
+
+console.log(woodCalculator(6, 1, 3));
+
+
+/* Assignment 3 */
+
+
+function brickCalculator(floor){
+    let totalFeet = 0;
+
+    if( floor <= 10 ){
+        totalFeet = floor * 15;
+    } else if ( floor <= 20 ){
+        let firstPart = 10 * 15;
+        let secondPart = (floor - 10) * 12;
+        totalFeet = firstPart + secondPart;
+    } else{
+        firstPart = 10 * 15;
+        secondPart = 10 * 12;
+        let thirdPart = (floor - 20) * 10;
+
+        totalFeet = firstPart + secondPart + thirdPart;
+    }
+
+    let totalBrick = totalFeet * 1000;
+    return totalBrick;
+}
+
+console.log(brickCalculator(33));
+
+
+/* Assignment 4 */
+
+
+
+let friends = ["Rahim", "Karim", "Mushfiq", "John", "Doe"];
+
+function tinyfriend(friends){
+    let tiny = friends[0];
+    for(let i = 1; i < friends.length; i++){
+        if(friends[i].length < tiny.length){
+            tiny = friends[i];
+        }
+        
+    };
+    return tiny;
+}
+
+console.log(tinyfriend(friends));

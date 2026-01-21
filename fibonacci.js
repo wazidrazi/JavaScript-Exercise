8ea8bbac896f8622acf2974ae74e74df30d99d8d
wazidrazi <wazidrazi20@gmail.com>
2026-01-21 22:57:56 +0600

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -5,32 +5,32 @@
 
 
 
-// function fibonacci(n){
-//     let fibo = [0, 1];
+function fibonacci(n){
+    let fibo = [0, 1];
 
-//     for(let i=2; i<= n; i++){
-//         fibo[i] = fibo[i-1] + fibo[i-2];
-//     }
-//     return fibo;
-// }
+    for(let i=2; i<= n; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo;
+}
 
-// let result = fibonacci(10);
-// console.log(result);
+let result = fibonacci(10);
+console.log(result);
 
 
 
 /* Recursive way */
 
 
-function fibonacci(n){
-    if(n == 0){
-        return 0;
-    } if(n == 1){
-        return 1;
-    } else{
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
-}
+// function fibonacci(n){
+//     if(n == 0){
+//         return 0;
+//     } if(n == 1){
+//         return 1;
+//     } else{
+//         return fibonacci(n-1) + fibonacci(n-2);
+//     }
+// }
 
-let result = fibonacci(10);
-console.log(result);
+// let result = fibonacci(10);
+// console.log(result);

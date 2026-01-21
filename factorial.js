333ddf329ae98845b973b63370911559472978c3
wazidrazi <wazidrazi20@gmail.com>
2026-01-21 22:56:39 +0600

    while factorial

--- factorial.js
+++ factorial.js
@@ -17,30 +17,30 @@
 
 // /* While */
 
-// function factorial(n){
-//     let fact = 1;
-//     let i = 1
-//     while(i <= 5){
-//         fact = fact * i;
-//         i++;
-//     }
-//     return fact;
-// }
+function factorial(n){
+    let fact = 1;
+    let i = 1
+    while(i <= 5){
+        fact = fact * i;
+        i++;
+    }
+    return fact;
+}
 
-// console.log(factorial(5));
+console.log(factorial(5));
 
 
 
 
 /* Recursive method */
 
-function factorial(n){
-    if(n == 1){
-        return 1;
-    }else{
-        return n * factorial(n - 1);
-    }
-}
+// function factorial(n){
+//     if(n == 1){
+//         return 1;
+//     }else{
+//         return n * factorial(n - 1);
+//     }
+// }
 
-let result = factorial(4);
-console.log(result);
+// let result = factorial(4);
+// console.log(result);

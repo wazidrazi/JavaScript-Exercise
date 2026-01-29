7130de0b415327fc7a51151b15f37f98c8c871fa
wazidrazi <wazidrazi20@gmail.com>
2026-01-29 23:12:22 +0600

    arguments name

--- argument.js
+++ argument.js
@@ -1,13 +1,26 @@
-function addNumbers(num1, num2){
+// function addNumbers(num1, num2){
 
-    let sum = 0;
+//     let sum = 0;
 
+//     for(let i = 0; i < arguments.length; i++){
+//         let num = arguments[i];
+//         sum = sum + num;
+//     }
+
+//     return sum;
+// }
+
+// console.log(addNumbers(5,6,9,32));
+
+
+
+function callName(name){
+    let userName = " ";
     for(let i = 0; i < arguments.length; i++){
-        let num = arguments[i];
-        sum = sum + num;
+        let borthersName = arguments[i];
+        userName = userName + borthersName;
     }
-
-    return sum;
+    return userName;
 }
 
-console.log(addNumbers(5,6,9,32));
+console.log(callName("rahim uddin",",", "kalim uddin",",", "jaber uddin",",", "manik uddin"));

2cb0cbe813d32b4b49f420809394d8192cbaa99e
wazidrazi <wazidrazi20@gmail.com>
2026-01-29 23:14:44 +0600

    callback function

--- callback.js
+++ callback.js
@@ -1,1 +1,16 @@
-console.log("callback")
+function callback(name, age, task){
+    console.log('Name:', name);
+    console.log('Age:', age);
+    task();
+}
+
+function business(){
+    console.log('Occupation: Business Man');
+}
+
+function serviceHolder(){
+    console.log('Occupation: Service Holder');
+}
+
+callback('Jashim Uddin', 26, business);
+callback('Ashik Hasan', 21, serviceHolder);

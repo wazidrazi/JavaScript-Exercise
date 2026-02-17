6a3b5b7b7b68e3979905ee4da0215f2268b78307
wazidrazi <wazidrazi20@gmail.com>
2026-02-17 21:05:16 +0600

    using call and apply on method

--- Js object/callApply.js
+++ Js object/callApply.js
@@ -5,12 +5,36 @@ const userInfo = {
     fullName : function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill : function(amount){
-        this.salary = this.salary - amount;
+    chargeBill : function(amount, tax){
+        console.log(this);
+        this.salary = this.salary - amount - tax;
         return this.salary
     }
 
 };
 
-userInfo.chargeBill(2000);
-console.log(userInfo.salary);
+const userInfo2 = {
+    firstName : "Rahim",
+    lastName : "Uddin",
+    salary : 20000
+};
+
+const userInfo3 = {
+    firstName : "Karim",
+    lastName : "Uddin",
+    salary : 25000
+};
+
+// userInfo.chargeBill(2000);
+// console.log(userInfo.salary);
+
+/* if we want to apply same method in different object, we will use call and apply */
+
+// userInfo.chargeBill.call(userInfo2, 2000);
+// console.log(userInfo2.salary);
+
+userInfo.chargeBill.apply(userInfo3, [5000, 200]);
+console.log(userInfo3.salary);
+
+
+
